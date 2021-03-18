4ecf6e2324e49d418fd513bdafbcca82aa0db3de
a2337230 <coolboyjrf1990@163.com>
2021-03-18 17:01:21 +0800

    '31'

--- 01server/app.js
+++ 01server/app.js
@@ -1,7 +1,7 @@
 const serverHandle = (req, res) => {
   // 设置返回JSON格式
   res.setHeader('Content-type', 'application/json')
-  
+  res.setHeader('Set-Cookie', 'a=1; path=/; httpOnly')
   const resData = {
     name: 12,
     password: 'j543540310',
---
+++ 03redis/index.js
@@ -0,0 +1,7 @@
+const redis = require('redis')
+
+const redisClient = redis.createClient(6379, '127.0.0.1')
+
+redisClient.on('error', err => {
+  console.log(err)
+})
