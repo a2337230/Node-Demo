57ad82c4459266cbd1d55d06392c1df7b0c4d02f
a2337230 <coolboyjrf1990@163.com>
2021-04-13 10:41:58 +0800

    '2134'

--- blog-koa2/routes/index.js
+++ blog-koa2/routes/index.js
@@ -2,12 +2,16 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 21!'
   })
 })
 
 router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
+  // ctx.body = 'koa2 string'
+  ctx.body = {
+    code: 200,
+    data: 'koa2 string'
+  }
 })
 
 router.get('/json', async (ctx, next) => {
