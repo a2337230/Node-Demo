db2301530bf74e5431b3c7180ebaba130ae2662f
a2337230 <coolboyjrf1990@163.com>
2021-04-15 10:43:52 +0800

    '213'

---
+++ mongodb/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('./../test2')
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true, // 必传
+    unique: true // 唯一
+  },
+  // username: String,
+  password: String,
+  realname: String
+})
+
+// Model 对应collection
+const User = mongoose.model('user', UserSchema)
